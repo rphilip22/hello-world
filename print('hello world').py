3ad17255e3608df1c795d6fd999563302b032273
rphilip22 <reuben.philip@student.fairfield.edu>
2026-01-28 15:30:45 -0500

    functions

--- print('hello world').py
+++ print('hello world').py
@@ -1,3 +1,5 @@
-name = input("What's your name?")
-print("Hello, " + name)
 #this program prompts the user for their name and greets them
+name = input("What's your name? ").title()
+clean = " ".join(name.split())
+print(f"Hello, {clean}")
+# or print("Hello," , name) or print(f"Hello, {name}")
