513d156aae6e497a3a38f39085a6fe65a381bb12
rphilip22 <reuben.philip@student.fairfield.edu>
2026-02-04 14:35:48 -0500

    Updated

--- calculator.py
+++ calculator.py
@@ -2,5 +2,4 @@ x = float(input("Input a number for x: "))
 y = float(input("Input a number for y: "))
 z = x+y
 print(z)
-#print("The sum of " + str(x) + " and " + str(y) + " = " + str(z)   
-# bla
+#print("The sum of " + str(x) + " and " + str(y) + " = " + str(z)
